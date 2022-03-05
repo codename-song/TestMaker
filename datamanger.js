68ee7ba6863c3c7c3a7665398e6fd20d5be5f575
codename_song <code_007@ymail.com>
2022-03-05 23:38:52 +0900

    Load Data from xlse file suffle all questions and make 20 questions Get data and print it test_largepart.html make question <div> 'alloctated'

--- datamanger.js
+++ datamanger.js
@@ -1,63 +1,126 @@
-
-let question =  {
+let question = {
+  type: "",
   name: "",
   frontImg: "",
-  imgList: ["","",""],
-  type: "",
+  imgList1: "",
+  imgList2: "",
+  imgList3: "",
   currentNum: 0,
 };
 
 var pig_questionList = new Array();
 var cow_questionList = new Array();
+let isAllLoaed = false;
 
-window.onload = function () {
+var testQuestion_List = new Array();
 
-  var url = "/datafiles/questionDB.xlsx";
-  var oReq = new XMLHttpRequest();
-  oReq.open("GET", url, true);
-  oReq.responseType = "arraybuffer";
+var tempData = question;
 
-  let loadedData = new Array();
+//window.onload = function loadAllData() {
 
-  oReq.onload = function(e) {
-    var arraybuffer = oReq.response;
+var url = "/datafiles/questionDB.xlsx";
+var oReq = new XMLHttpRequest();
+oReq.open("GET", url, true);
+oReq.responseType = "arraybuffer";
 
-    /* convert data to binary string */
-    var data = new Uint8Array(arraybuffer);
-    var arr = new Array();
-    for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
-    var bstr = arr.join("");
+let loadedData = new Array();
 
-    /* Call XLSX */
-    var workbook = XLSX.read(bstr, {type:"binary"});
+oReq.onload = function (e) {
+  pig_questionList = [];
+  cow_questionList = [];
+  var arraybuffer = oReq.response;
 
-    /* DO SOMETHING WITH workbook HERE */
-    var first_sheet_name = workbook.SheetNames[0];
-    /* Get worksheet */
-    var worksheet = workbook.Sheets[first_sheet_name];
-    console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
-    loadedData = XLSX.utils.sheet_to_json(worksheet,{raw:true});
+  /* convert data to binary string */
+  var data = new Uint8Array(arraybuffer);
+  var arr = new Array();
+  for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+  var bstr = arr.join("");
 
-    for(var i = 0 ; i < loadedData.length ; i++){
+  /* Call XLSX */
+  var workbook = XLSX.read(bstr, { type: "binary" });
 
-      var tempData = question;
-  
-      tempData.name = loadedData[i].name;
-      tempData.type = loadedData[i].type;
-      tempData.frontImg = loadedData[i].imgList1;
-      tempData.imgList[0] = loadedData[i].imgList1;
-      tempData.imgList[1] = loadedData[i].imgList1; // 임시로 1로만 지정
-      tempData.imgList[2] = loadedData[i].imgList1; // 임시로 1로만 지정
+  /* DO SOMETHING WITH workbook HERE */
+  var first_sheet_name = workbook.SheetNames[0];
+  /* Get worksheet */
+  var worksheet = workbook.Sheets[first_sheet_name];
+  console.log(XLSX.utils.sheet_to_json(worksheet, { raw: true }));
+  loadedData = XLSX.utils.sheet_to_json(worksheet, { raw: true });
 
-      if(loadedData[i].type == "돼지"){
-        pig_questionList.push(tempData);
+  if (!isAllLoaed) {
+    for (var i = 0; i < loadedData.length; i++) {
+      if (loadedData[i].type == "돼지") {
+        pig_questionList.push(loadedData[i]);
+        pig_questionList[i].frontImg = pig_questionList[i].imgList1;
+      } else if (loadedData[i].type == "소") {
+        cow_questionList.push(loadedData[i]);
+        cow_questionList[cow_questionList.length - 1].frontImg =
+          cow_questionList[cow_questionList.length - 1].imgList1;
+      } else {
+        console.log("wWTFFFFFF");
       }
-      else if (loadedData[i].type == "소"){
-        cow_questionList.push(tempData);
+      if (i >= loadedData.length - 1) {
+        isAllLoaed = true;
+        console.log("all data have been loaded");
+        console.log(pig_questionList);
+        console.log(cow_questionList);
       }
     }
   }
 
-  oReq.send();
+  if (isAllLoaed) {
+    setQuestion();
+  }
+};
+oReq.send();
+//}
 
+function setQuestion() {
+  if (!isAllLoaed) return;
+  console.log("wait for data load");
+  if (pig_questionList[0] != null) {
+    document.getElementsByClassName("question-column__image")[0].src =
+      pig_questionList[0].frontImg;
+    console.log(
+      "try to change src : " +
+        document.getElementsByClassName("question-column__image")[0].src
+    );
+  } else {
+    console.log("it is null");
+  }
+
+  // 총 20개의 문제 출제
+  // 돼지에서 10개 랜덤추출
+  // 소 에서 10개 랜덤 추출
+  // 20개의 리스트에 다 집어 넣은 뒤, 배열 섞기
+  // for 문 돌려서 문제 만들기
+
+  pig_questionList.sort(() => Math.random() - 0.5);
+  cow_questionList.sort(() => Math.random() - 0.5);
+
+  for (i = 0; i < 10; i++) {
+    testQuestion_List.push(pig_questionList[i]);
+    testQuestion_List.push(cow_questionList[i]);
+  }
+
+  testQuestion_List.sort(() => Math.random() - 0.5);
+
+  console.log(testQuestion_List);
+
+  for (i = 0; i < testQuestion_List.length; i++) {
+    testQuestion_List[i].currentNum = i + 1;
+    createQuestionElement(testQuestion_List[i]);
+  }
+}
+
+function createQuestionElement(tempData) {
+  // create question html divs
+  var qdiv = document.createElement("div");
+  qdiv.innerHTML =
+    document.getElementsByClassName("question-column")[0].innerHTML;
+
+  qdiv.getElementsByClassName("question-column__image")[0].src =
+    tempData.frontImg;
+  qdiv.getElementsByClassName("question-column__number")[0].innerHTML =
+    tempData.currentNum;
+  document.getElementById("question-field").appendChild(qdiv);
 }
